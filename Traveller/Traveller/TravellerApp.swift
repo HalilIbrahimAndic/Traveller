5c9a5a9ec7e4bdab3bf4863328280595cd71d031
Halil İbrahim Andiç <hiandic@gmail.com>
2024-01-27 12:24:08 +0300

    Add: Project

---
+++ Traveller/Traveller/TravellerApp.swift
@@ -0,0 +1,17 @@
+//
+//  TravellerApp.swift
+//  Traveller
+//
+//  Created by handic on 27.01.2024.
+//
+
+import SwiftUI
+
+@main
+struct TravellerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
